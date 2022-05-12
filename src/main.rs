175ac2b66bece03c73f88063e729b45102112329
Raena <raen@quantumtrip.org>
2022-05-12 00:01:38 -0400

    Start using bevy_egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {            
+            title: "AutoRPG".to_string(),
+            ..default()         
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        //.add_startup_system(setup)
+        .add_system(ui_example)
+        .run();
+}
+
+//fn _setup(mut commands: Commands, asset_server: Res<AssetServer>) {}
+
+fn ui_example(mut egui_ctx: ResMut<EguiContext>) {
+    egui::CentralPanel::default().show(egui_ctx.ctx_mut(), |ui| {
+        if ui.button("Click me").clicked() {
+            
+        }
+    });
 }
