31291ba891b61ae8e557b117203f38c6d8e22910
Raena <raen@quantumtrip.org>
2022-05-28 14:18:50 -0400

    New blank GUI

--- game_plugin/src/lib.rs
+++ game_plugin/src/lib.rs
@@ -8,12 +8,6 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app
-            .init_resource::<GameState>()
             .add_plugin(ui_plugin::UiPlugin);
     }
-}
-
-#[derive(Default)]
-pub struct GameState {
-    click_count: i32,
 }
--- game_plugin/src/ui_plugin.rs
+++ game_plugin/src/ui_plugin.rs
@@ -1,8 +1,5 @@
 use bevy::prelude::{App, Plugin, ResMut};
-use bevy_egui::{
-    egui::{self, Align2},
-    EguiContext,
-};
+use bevy_egui::{egui, EguiContext};
 
 pub struct UiPlugin;
 
@@ -12,30 +9,8 @@ impl Plugin for UiPlugin {
     }
 }
 
-fn ui_system(mut egui_ctx: ResMut<EguiContext>, game_state: ResMut<crate::GameState>) {
-    egui::Window::new("")
-        .anchor(Align2::CENTER_CENTER, (0., 0.))
-        .title_bar(false)
-        .resizable(false)
-        .collapsible(false)
-        .show(egui_ctx.ctx_mut(), |ui| {
-            add_clicker(ui, game_state);
-        });
-}
+fn ui_system(mut egui_ctx: ResMut<EguiContext>) {
+    egui::CentralPanel::default().show(egui_ctx.ctx_mut(), |_ui| {
 
-fn add_clicker(ui: &mut egui::Ui, mut game_state: ResMut<crate::GameState>) {
-    ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
-        if ui
-            .add_sized(
-                [200.0, 100.0],
-                egui::Button::new(egui::RichText::new("Click me!").size(32.0)),
-            )
-            .clicked()
-        {
-            game_state.click_count += 1;
-        }
-
-        let click_text = format!("You've clicked {} times!", game_state.click_count);
-        ui.label(egui::RichText::new(click_text).size(32.0));
     });
 }
